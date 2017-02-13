95aef3e8956ecc4d8059f1a9da1158027d23f952
samsam895 <mobappsam@gmail.com>
2017-02-13 10:47:50 -0500

    First commit

---
+++ app/src/main/java/mejia/sam/takeashot/MainActivity.java
@@ -0,0 +1,37 @@
+package mejia.sam.takeashot;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    int TAKE_PHOTO_CODE = 0;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, TAKE_PHOTO_CODE);
+
+                }
+            }
+        });
+    }
+}
